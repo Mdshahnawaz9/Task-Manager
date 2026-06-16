35fec5ef3afbd5ecb1891e0b58abc6c086e730a0
Mdshahnawaz9 <mdshahnawaz090605@gmail.com>
2026-06-17 04:22:21 +0530

    Add start script

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const taskRoutes = require("./routes/taskRoutes");
 
 app.use("/tasks", taskRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`);
